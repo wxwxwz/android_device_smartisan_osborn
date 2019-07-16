47dc6a52b6bf1b51d6f150f7b38060c7c946e975
Sujin Panicker <spanic@codeaurora.org>
2019-07-16 18:26:34 +0530

    osborn: media: Fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: Ided16f454fcb3b760076500e87759bcbb44f75ed

--- properties.mk
+++ properties.mk
@@ -15,6 +15,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
