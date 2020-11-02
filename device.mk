f8d6a1ad6195563e32d01cd6b099c35a417b2d63
Bruno Martins <bgcngm@gmail.com>
2020-11-02 17:40:19 +0000

    osborn: Update NQ NFC packages for R bringup
    Change-Id: I618104845ad1ccd5cc265cdbc94c9118fcaa199e

--- device.mk
+++ device.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw \
-    vendor.nxp.hardware.nfc@1.2-service
+    vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
