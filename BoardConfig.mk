e24545b3983a320c36316b380a24b5790976142a
Bruno Martins <bgcngm@gmail.com>
2021-12-03 13:35:30 +0800

    osborn: Remove deprecated telephony flag
    The custom jar is dead for good in Android 11.
    
    Change-Id: I7f0f7afd186b00f01183bf4479092d03897e074a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 BOARD_SUPPRESS_EMMC_WIPE := true
 
 # RIL
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_LD_SHIM_LIBS += /system/lib/lib-imsvideocodec.so|/system/lib/libshim_ims.so
 TARGET_LD_SHIM_LIBS += /system/lib64/lib-imsvideocodec.so|/system/lib64/libshim_ims.so
