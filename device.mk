06ff53567e187edd2890589718d636d692b3f8c1
anrui2032 <anrui2032@gmail.com>
2022-01-22 11:49:23 +0800

    osborn: Build deprecated radio 1.0 interface
    Change-Id: I0a5bd8b76be562976b58fcf5f04e479e80d7c200

--- device.mk
+++ device.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     libshim_ims \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
     ims-ext-common \
     librmnetctl \
