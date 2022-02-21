42612cac11458d60e5a7b3243b96ef1a9a107690
anrui2032 <anrui2032@gmail.com>
2022-02-21 12:39:37 +0800

    osborn: Build keymaster 3.0 service
    Change-Id: I53f299d27ec83ebfa0b0951aca57827a209cdddb

--- device.mk
+++ device.mk
@@ -282,10 +282,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Keymaster / Keystore
+# Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
