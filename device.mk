fc5985ef281f1aa433bde184a8905fd7278cd418
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-13 05:05:07 +0800

    osborn: Use libprotobuf-cpp-full-v29.so for libsettings.so
    * The symbol `_ZN6google8protobuf8internal13empty_string_E` was
      removed in the API30 version of this lib. Copy the old one
      and patchelf in the dependency.
    
    Change-Id: I3553c9cbb6ccff202aab706b7b7fa2f39d1480a9

--- device.mk
+++ device.mk
@@ -456,6 +456,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
