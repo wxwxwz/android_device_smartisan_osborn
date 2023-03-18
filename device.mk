e6b77955e9e7e53e66d827f053fcf808475882fd
guaiyihu <guaiyihu@foxmail.com>
2023-03-18 06:06:27 +0000

    osborn: rootdir: Clean up unused init scripts
     * init.qcom.class_core.sh
     * init.qcom.coex.sh
     * init.qcom.crashdata.sh
     * init.qcom.efs.sync.sh
     * init.qcom.sdio.sh
     * init.qcom.syspart_fixup.sh
     * init.qcom.wifi.sh
     * init.qti.ims.sh
     * qca6234-service.sh
    
    Change-Id: Ib6ba59acb1f1d6b773aeca3c326944049784887c

--- device.mk
+++ device.mk
@@ -252,20 +252,11 @@ PRODUCT_PACKAGES += \
     init.crda.sh \
     init.mdm.sh \
     init.qcom.sh \
-    init.qcom.class_core.sh \
-    init.qcom.coex.sh \
-    init.qcom.crashdata.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
     init.qcom.sensors.sh \
-    init.qcom.syspart_fixup.sh \
     init.qcom.ssr.sh \
     init.qcom.usb.sh \
-    init.qcom.wifi.sh \
-    init.qti.ims.sh \
-    qca6234-service.sh \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
