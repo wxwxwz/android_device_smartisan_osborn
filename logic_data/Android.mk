4751917a99fa51e1f3210a932a3b41073b45d976
anrui2032 <anrui2032@gmail.com>
2023-04-04 02:10:47 +0000

    osborn: Move logic_volume daemon to init
    Change-Id: I62ba9bbd4fb2cd7359be753c40c8b11bd01417af

--- logic_data/Android.mk
+++ logic_data/Android.mk
@@ -31,7 +31,7 @@ LOCAL_MODULE       := logic_volume.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/logic_volume.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
